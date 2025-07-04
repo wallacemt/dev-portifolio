0b7916d4ce34ab01eef447f67f4862499dd62ccc
Wallace Santana <wallacesantanak0@gmail.com>
2025-07-04 19:00:55 -0300

    refractor(./**): dividing project structure into different repositories

--- backend/src/tests/mocks/prismaMock.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended';
-
-export const prismaMock = mockDeep<PrismaClient>() as unknown as DeepMockProxy<PrismaClient>;
-
-beforeEach(() => {
-  mockReset(prismaMock);
-});
