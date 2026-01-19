6de19878daf7de0174a0b6fd9f9cf7afb3f11687
Wallace Santana <wallacesantanak0@gmail.com>
2026-01-19 14:37:48 -0300

    feat(./badge-card): update card add scroll

--- src/components/Visitor/Formations/_components/badge-card.tsx
+++ src/components/Visitor/Formations/_components/badge-card.tsx
@@ -8,6 +8,7 @@ import { ptBR, enUS } from "date-fns/locale";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { RotateImage } from "@/components/ui/rotate-image";
+import { ScrollArea } from "../../../ui/scroll-area";
 
 interface BadgeCardProps {
   badge: BadgeType;
@@ -28,19 +29,23 @@ export function BadgeCard({ badge, language = "pt", className }: BadgeCardProps)
       <CardHeader className="pb-3">
         <div className="flex flex-col items-center justify-between gap-2">
           {badge.imageUrl && <RotateImage imageUrl={badge.imageUrl} title={badge.title} />}
-          <div className="flex-1 space-y-1">
-            <CardTitle className="text-lg line-clamp-2 group-hover:text-primary transition-colors">
-              {badge.title}
-            </CardTitle>
-            <CardDescription className="flex items-center gap-1 text-xs">
-              <Award className="h-4 w-4" />
-              {badge.issuer}
-            </CardDescription>
-          </div>
+          <ScrollArea className="h-[112px] w-full  p-4">
+            <div className="flex-1 space-y-1">
+              <CardTitle className="text-lg line-clamp-2 group-hover:text-primary transition-colors">
+                {badge.title}
+              </CardTitle>
+              <CardDescription className="flex items-center gap-1 text-xs">
+                <Award className="h-4 w-4" />
+                {badge.issuer}
+              </CardDescription>
+            </div>
+          </ScrollArea>
         </div>
       </CardHeader>
       <CardContent className="space-y-3">
-        <p className="text-sm text-muted-foreground line-clamp-3">{badge.description}</p>
+        <ScrollArea className="h-[120px] w-full  p-4">
+          <p className="text-sm text-muted-foreground line-clamp-3">{badge.description}</p>
+        </ScrollArea>
 
         <div className="flex items-center justify-between pt-2 border-t border-border/50">
           <div className="flex items-center gap-1.5 text-xs text-muted-foreground">
